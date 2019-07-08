9a75323526c0e2036a36f6927a3e5bebe017a62a
Eduardo Verdeja <eaverdeja@gmail.com>
2019-07-08 00:24:25 +0000

    Adding JavaScript

---
+++ assets/js/player.js
@@ -0,0 +1,28 @@
+let Player = {
+    player: null,
+
+    init(domId, playerId, onReady) {
+        window.onYouTubeIframeAPIReady = () => {
+            this.onIframeReady(domId, playerId, onReady)
+        }
+        let youtubeScriptTag = document.createElement("script")
+        youtubeScriptTag.src = "https://youtube.com/iframe_api"
+        document.head.appendChild(youtubeScriptTag)
+    },
+    onIframeReady(domId, playerId, onReady) {
+        this.player = new YT.Player(domId, {
+            height: "360",
+            width: "420",
+            videoId: playerId,
+            events: {
+                "onReady": onReady,
+                "onStateChange": event => this.onPlayerStateChange(event)
+            }
+        })
+    },
+    onPlayerStateChange(event) { },
+    getCurrentTime: () => Math.floor(this.player.getCurrentTime() * 1000),
+    seekTo: millsec => this.player.seekTo(millsec / 1000)
+}
+
+export default Player
