dd6a0b1d7b5922a33eb8a16a85882834199fa1be
Eduardo Verdeja <eaverdeja@gmail.com>
2019-07-14 13:38:27 +0000

    Handling Disconnects

--- assets/js/video.js
+++ assets/js/video.js
@@ -14,7 +14,10 @@ let Video = {
         let msgContainer = document.getElementById("msg-container")
         let msgInput = document.getElementById("msg-input")
         let postButton = document.getElementById("msg-submit")
-        let vidChannel = socket.channel("videos:" + videoId)
+        let lastSeenId = 0
+        let vidChannel = socket.channel("videos:" + videoId, () => ({
+            last_seen_id: lastSeenId
+        }))
 
         postButton.addEventListener("click", e => {
             let payload = { body: msgInput.value, at: Player.getCurrentTime() }
@@ -26,7 +29,10 @@ let Video = {
             msgInput.value = ""
         })
 
-        vidChannel.on("new_annotation", resp => this.renderAnnotation(msgContainer, resp))
+        vidChannel.on("new_annotation", resp => {
+            lastSeenId = resp.id
+            this.renderAnnotation(msgContainer, resp)
+        })
 
         msgContainer.addEventListener("click", e => {
             e.preventDefault()
@@ -40,6 +46,8 @@ let Video = {
 
         vidChannel.join()
             .receive("ok", ({ annotations }) => {
+                let ids = annotations.map(ann => ann.id)
+                if (ids.length > 0) { lastSeenId = Math.max(...ids) }
                 this.scheduleMessages(msgContainer, annotations)
             })
             .receive("error", reason => console.log("join failed", reason))
