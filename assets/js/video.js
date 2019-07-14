ffc95169edfb7cc8987f0f5501db2d4a9339b2e7
Eduardo Verdeja <eaverdeja@gmail.com>
2019-07-14 13:26:30 +0000

    Persisting Annotations

--- assets/js/video.js
+++ assets/js/video.js
@@ -28,8 +28,20 @@ let Video = {
 
         vidChannel.on("new_annotation", resp => this.renderAnnotation(msgContainer, resp))
 
+        msgContainer.addEventListener("click", e => {
+            e.preventDefault()
+            let seconds = e.target.getAttribute("data-seek")
+                || e.target.parentNode.getAttribute("data-seek")
+
+            if (!seconds) { return }
+
+            Player.seekTo(seconds)
+        })
+
         vidChannel.join()
-            .receive("ok", resp => console.log("joined the video channel", resp))
+            .receive("ok", ({ annotations }) => {
+                this.scheduleMessages(msgContainer, annotations)
+            })
             .receive("error", reason => console.log("join failed", reason))
     },
 
@@ -38,12 +50,39 @@ let Video = {
 
         template.innerHTML = `
         <a href="#" data-seek="${this.esc(at)}">
+            [${this.formatTime(at)}]
             <b>${this.esc(user.username)}</b>: ${this.esc(body)}
         </a>
         `
 
         msgContainer.appendChild(template)
         msgContainer.scrollTop = msgContainer.scrollHeight
+    },
+
+    scheduleMessages(msgContainer, annotations) {
+        clearTimeout(this.scheduleTimer)
+        this.scheduleTimer = setTimeout(() => {
+            let ctime = Player.getCurrentTime()
+            let remaining = this.renderAtTime(annotations, ctime, msgContainer)
+            this.scheduleMessages(msgContainer, remaining)
+        }, 1000)
+    },
+
+    renderAtTime(annotations, seconds, msgContainer) {
+        return annotations.filter(ann => {
+            if (ann.at >= seconds) {
+                return true
+            } else {
+                this.renderAnnotation(msgContainer, ann)
+                return false
+            }
+        })
+    },
+
+    formatTime(at) {
+        let date = new Date(null)
+        date.setSeconds(at / 1000)
+        return date.toISOString().substr(14, 5)
     },
 
     esc(str) {
